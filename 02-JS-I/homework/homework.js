cf3a4920b9b23425f68a3879ff0f9bad333bd4c0
Fabrizio Franceschini <98345343+fabrifran93@users.noreply.github.com>
2022-02-02 17:20:19 -0300

    homework js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -26,33 +26,27 @@ const nuevoModulo = 21 % 5 === null;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
-  // "Return" la string provista: str
-  // Tu código:
+  return "str"
   
 }
 
 function suma(x, y) {
-  // "x" e "y" son números
-  // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código:
+  return x + y;
   
 }
 
 function resta(x, y) {
-  // Resta "y" de "x" y devuelve el valor
-  // Tu código:
+  return y - x;
   
 }
 
 function multiplica(x, y) {
-  // Multiplica "x" por "y" y devuelve el valor
-  // Tu código:
+  return x * y;
   
 }
 
 function divide(x, y) {
-  // Divide "x" entre "y" y devuelve el valor
-  // Tu código:
+ return x / y ;
   
 }
 
