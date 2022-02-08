7727a237feb87c586017329493e026746a0126ca
Fabrizio Franceschini <98345343+fabrifran93@users.noreply.github.com>
2022-02-08 18:05:20 -0300

    homework js4

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -164,6 +164,10 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  producto.calcularPrecioDescuento = function(){
+    return producto.precio - (producto.precio * producto.porcentajeDeDescuento)
+  };
+  return  producto;
 
 }
 
