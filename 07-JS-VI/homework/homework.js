520256b685e5253d50f36b01bca3e560057e6d91
Fabrizio Franceschini <98345343+fabrifran93@users.noreply.github.com>
2022-02-11 17:04:28 -0300

    homework js6

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -5,14 +5,14 @@ function mayuscula(nombre) {
   //pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return nombre[0].toUpperCase() + nombre.slice[1]
+  return nombre[0].toUpperCase() + nombre.slice(1);
   
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-  
+  cb()
 
 }
 
@@ -21,7 +21,8 @@ function operacionMatematica(n1, n2, cb) {
   //como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-  function callback ()
+    return cb(n1,n2)
+  
 }
 
 function sumarArray(numeros, cb) {
@@ -29,12 +30,21 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  //numeros = [1,2,3,4]
+  var suma = numeros.reduce(function(acc,curr){
+    return acc + curr
+
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(element){
+    cb(element)
+  });
 }
 
 function map(array, cb) {
@@ -42,12 +52,24 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var array1 = array.map(function(e){
+    return cb(e)
+  }
+  )
+  return array1
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var nuevoArray = [];
+  for (let i=0;i<array.length;i++){
+    if (array[i][0]=== 'a'){
+      nuevoArray.push(array[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 // No modificar nada debajo de esta línea
